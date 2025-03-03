dfea31bdb2c2f8f142f76356c417e98d84d6a17e
Ayush <ayusharyaneth@gmail.com>
2025-03-03 12:04:52 +0530

    Create about.js

---
+++ components/about.js
@@ -0,0 +1,8 @@
+export default function About() {
+  return (
+    <section id="about" className="p-10">
+      <h2 className="text-3xl font-bold">About Me</h2>
+      <p className="mt-4">I have been in **crypto & forex trading** for over 2 years and **airdrop farming** for 3+ years. I also enjoy coding and learning new technologies.</p>
+    </section>
+  )
+}
